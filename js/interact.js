93a0f54ed9adee5ace218375c1ff315921b81293
Ye Shin <yeshin316@gmail.com>
2017-04-15 19:43:21 -0400

    Updated underlines
    Added underlines to elements that can be clicked so it is easily visible
    to the user which elements can be clicked

--- js/interact.js
+++ js/interact.js
@@ -10,27 +10,49 @@ function fadeInEntry() {
 
 function labelThreeD() {
 	this.className = 'labelThreeD';
+
+	underline.className = '';
+}
+
+function flash() {
+	var underline = document.getElementById('underline');
+
+	underline.className = 'flash';
 }
 
 function fadeInUp() {
-	var about, work, resume, email, smLinks;
+	var about, work, resume, email, smLinks, contactLine, resumeLine;
 
 	about = document.getElementById('about');
 	work = document.getElementById('work');
 	resume = document.getElementById('resume');
 	email = document.getElementById('email');
 	smLinks = document.getElementById('sm-links');
+	contactLine = document.getElementById('contact-line');
+	resumeLine = document.getElementById('resume-line');
 
 	about.className = 'fadeInUp';
 	work.className = 'fadeInUp';
 	resume.className = 'fadeInUp';
 	email.className = 'fadeInUp';
 	smLinks.className = 'fadeInUp';
+	contactLine.className = 'fadeInUp';
+	resumeLine.className = 'fadeInUp';
+}
+
+function flashLines() {
+	contactLine = document.getElementById('contact-line');
+	resumeLine = document.getElementById('resume-line');
+
+	contactLine.className = 'flashContactLine';
+	resumeLine.className = 'flashResumeLine'
 }
 
 window.addEventListener('load', fadeInEntry, false);
+window.addEventListener('load', flash, false);
 label.addEventListener('click', labelThreeD, false);
 label.addEventListener('click', fadeInUp, false);
+label.addEventListener('click', flashLines, false);
 
 // var strings, i;
 
