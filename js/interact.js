faadcd9c6e9787001029ddc6f2b89bcbf623fa5b
Ye Shin <yeshin316@gmail.com>
2017-04-17 13:55:45 -0400

    Took out underlines
    Took out the underlines because they were being a pain to make flexible
    so just used border-bottom instead and animated that on hover. Much
    easier

--- js/interact.js
+++ js/interact.js
@@ -1,7 +1,7 @@
 // JavaScript for Ye Heon Shin's Portfolio
 
 function fadeInUp() {
-	var label, about, work, resume, email, smLinks, contactLine, resumeLine;
+	var label, about, work, resume, email, smLinks;
 
 	label = document.getElementById('label');
 	about = document.getElementById('about');
@@ -9,8 +9,6 @@ function fadeInUp() {
 	resume = document.getElementById('resume');
 	email = document.getElementById('email');
 	smLinks = document.getElementById('sm-links');
-	contactLine = document.getElementById('contact-line');
-	resumeLine = document.getElementById('resume-line');
 
 	label.className = 'fadeInEntry';
 	about.className = 'fadeInUp';
@@ -22,24 +20,5 @@ function fadeInUp() {
 	resumeLine.className = 'fadeInUp';
 }
 
-function flashLines() {
-	contactLine = document.getElementById('contact-line');
-	resumeLine = document.getElementById('resume-line');
-
-	contactLine.className = 'flashContactLine';
-	resumeLine.className = 'flashResumeLine'
-}
-
 window.addEventListener('load', fadeInUp, false);
-window.addEventListener('load', flashLines, false);
-
-// var strings, i;
-
-// function expandWeb() {
-// 	for (var i = 0; i < strings.length; i++) {
-// 		strings[i].className += ' string' + [i + 1];
-// 	}
-// }
-
-// label.addEventListener('mouseenter', expandWeb, false);
 
