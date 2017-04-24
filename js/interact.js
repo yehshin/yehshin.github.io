880fe42639006ff49ac65c48d3352846b7fc1119
Ye Shin <yeshin316@gmail.com>
2017-04-23 21:38:45 -0400

    Complete redesign of porfolio
    Completely redesigned my online portfolio to better show my design taste
    programming abilities

--- js/interact.js
+++ js/interact.js
@@ -1,24 +1,114 @@
 // JavaScript for Ye Heon Shin's Portfolio
 
-function fadeInUp() {
-	var label, about, work, resume, email, smLinks;
+var aboutLink = document.querySelector('.navAbout');
+var aboutBg = document.getElementById('aboutBg');
+var aboutText = document.getElementById('aboutText');
 
-	label = document.getElementById('label');
-	about = document.getElementById('about');
-	work = document.getElementById('work');
-	resume = document.getElementById('resume');
-	email = document.getElementById('email');
-	smLinks = document.getElementById('sm-links');
+var workLink = document.querySelector('.navWork');
+var workBg = document.getElementById('workBg');
+var workText = document.getElementById('workText');
 
-	label.className = 'fadeInEntry';
-	about.className = 'fadeInUp';
-	work.className = 'fadeInUp';
-	resume.className = 'fadeInUp';
-	email.className = 'fadeInUp';
-	smLinks.className = 'fadeInUp';
-	contactLine.className = 'fadeInUp';
-	resumeLine.className = 'fadeInUp';
+var contactLink = document.querySelector('.navContact');
+var contactBg = document.getElementById('contactBg');
+var resume = document.getElementById('resume');
+var email = document.getElementById('email');
+
+var stripedBg = document.getElementById('stripedBg');
+var aboutStripedBg = document.getElementById('aboutStripedBg');
+var workStripedBg = document.getElementById('workStripedBg');
+var contactStripedBg = document.getElementById('contactStripedBg');
+
+var platformTextBg = document.getElementById('platformTextBg');
+
+var loadCover = document.getElementById('loadCover');
+
+function loadPage() {
+
+	workBg.id = "workBgTop";
+	workText.id = "workBgTop";
+
+	contactBg.id = "contactBgTop";
+	resume.id = "hideResume";
+	email.id = "hideEmail";
+
+	workStripedBg.id = "workStripedBgTop";
+	contactStripedBg.id = "contactStripedBgTop";
+
+	stripedBg.id = "aboutStripedBg";
+	platformTextBg.id = "aboutPlatformTextBg";
+
+	loadCover.id = "loadCover";
+
 }
 
-window.addEventListener('load', fadeInUp, false);
+function about() {
 
+	if (workBg.id === "workBg" || contactBg.id === "contactBg") {
+		
+		aboutBg.id = "aboutBg";
+		aboutText.id = "aboutText";
+
+		workBg.id = "workBgTop";
+
+		contactBg.id = "contactBgTop";
+		resume.id = "resume";
+		email.id = "email";
+
+		aboutStripedBg.id = "aboutStripedBg";
+		workStripedBg.id = "workStripedBgTop";
+		contactStripedBg.id = "contactStripedBgTop";
+
+		stripedBg.id = "aboutStripedBg";
+		platformTextBg.id = "aboutPlatformTextBg";
+
+	}
+}
+
+function work() {
+
+	if (aboutBg.id === "aboutBg" || contactBg.id === "contactBg") {
+
+		workBg.id = "workBg";
+		workText.id = "workText";
+
+		aboutBg.id = "aboutBgTop";
+
+		contactBg.id = "contactBgTop";
+		resume.id = "resume";
+		email.id = "email";
+
+		workStripedBg.id = "workStripedBg";
+		aboutStripedBg.id = "aboutStripedBgTop";
+		contactStripedBg.id = "contactStripedBgTop";
+
+		stripedBg.id = "workStripedBg";
+		platformTextBg.id = "workPlatformTextBg";
+
+	}
+}
+
+function contact() {
+	
+	if (aboutBg.id === "aboutBg" || workBg.id === "workBg") {
+
+		contactBg.id = "contactBg";
+		resume.id = "resume";
+		email.id = "email";
+
+		aboutBg.id = "aboutBgTop";
+		workBg.id = "workBgTop";
+
+		contactStripedBg.id = 'contactStripedBg';
+		aboutStripedBg.id = "aboutStripedBgTop";
+		workStripedBg.id = "workStripedBgTop";
+
+		stripedBg.id = "contactStripedBg";
+		platformTextBg.id = "contactPlatformTextBg";
+
+	}
+}
+
+window.addEventListener("load", loadPage, false);
+aboutLink.addEventListener("click", about, false);
+workLink.addEventListener("click", work, false);
+contactLink.addEventListener("click", contact, false);
