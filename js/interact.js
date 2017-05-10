46de817675ba6c5589813a9dbf94ac1b5fab65d2
Ye Shin <yeheonshin@Yes-MacBook-Pro.local>
2017-05-10 03:01:38 -0400

    Complete redesign of portfolio
    Completely redesigned portfolio and also added new project, Jot It

--- js/interact.js
+++ js/interact.js
@@ -1,138 +1,21 @@
 // JavaScript for Ye Heon Shin's Portfolio
 
-function animeLinks() {
+function fadeIn() {
 
-	if (window.innerWidth > 500) {
-		var links, i;
+	var container, containerJotIt, pageLink;
 
-		links = document.querySelectorAll('a');
+	containerWork = document.querySelector("#container-work");
+	containerJotIt = document.querySelector("#container-jot-it");
+	pageLink = document.querySelector(".page-link")
 
-		for (i = 0; i < links.length; i++) {
-			links[i].addEventListener("mouseenter", function() {
-				this.className = "hovered";
-			}, false)
-
-			links[i].addEventListener("mouseleave", function() {
-				this.className = "";
-			}, false)
-		} 
+	if (document.title == "Ye Shin | Designer" || document.title == "Ye Shin | Designer | Work") {
+		containerWork.className = "fadeInUp";
+		pageLink.className += " fadeIn";
+	} else {
+		containerJotIt.className = "fadeInUp";
+		pageLink.className += " fadeIn";
 	}
 
-} // closes: function animeLinks()
+}
 
-function flipLanding() {
-
-	var landingText, i;
-
-	landingText = document.querySelectorAll('.landingText');
-
-	for (i = 0; i < landingText.length; i++) {
-		landingText[i].className += " flipInX";
-	}
-
-} // closes: function flipLanding
-
-function fadeInText() {
-
-	var aboutHead, aboutText, workHead, workText, contactHead, contactText;
-
-	aboutHead = document.querySelector('.aboutHead');
-	aboutText = document.querySelectorAll('.aboutText');
-
-	workHead = document.querySelector('.workHead');
-	workText = document.querySelectorAll('.workText');
-
-	contactHead = document.querySelector('.contactHead');
-	contactText = document.querySelectorAll('.contactText');
-
-	if (window.innerWidth > 667) { // desktop
-
-		if (window.pageYOffset > 350) {
-			aboutHead.className += " fadeInUp";
-			aboutText[0].className += " fadeInUp";
-			aboutText[1].className += " fadeInUp";
-			aboutText[2].className += " fadeInUp";
-		}
-
-		if (window.pageYOffset > 800) {
-			workHead.className += " fadeInUp";
-			workText[0].className += " fadeInUp";
-			workText[1].className += " fadeInUp";
-			workText[2].className += " fadeInUp";			
-			workText[3].className += " fadeInUp";
-			workText[4].className += " fadeInUp";
-			workText[5].className += " fadeInUp";
-		}
-
-		if (window.pageYOffset > 1200) {
-			contactHead.className += " fadeInUp";
-			contactText[0].className += " fadeInUp";
-			contactText[1].className += " fadeInUp";
-			contactText[2].className += " fadeInUp";
-			contactText[3].className += " fadeInUp";
-			contactText[4].className += " fadeInUp";
-		}
-
-	} else if (window.innerWidth <= 375) { // mobile | portrait
-
-		if (window.pageYOffset > 300) {
-			aboutHead.className += " fadeInUp";
-			aboutText[0].className += " fadeInUp";
-			aboutText[1].className += " fadeInUp";
-			aboutText[2].className += " fadeInUp";
-		}
-
-		if (window.pageYOffset > 700) {
-			workHead.className += " fadeInUp";
-			workText[0].className += " fadeInUp";
-			workText[1].className += " fadeInUp";
-			workText[2].className += " fadeInUp";
-			workText[3].className += " fadeInUp";
-			workText[4].className += " fadeInUp";
-			workText[5].className += " fadeInUp";
-		}
-
-		if (window.pageYOffset > 1200) {
-			contactHead.className += " fadeInUp";
-			contactText[0].className += " fadeInUp";
-			contactText[1].className += " fadeInUp";
-			contactText[2].className += " fadeInUp";
-			contactText[3].className += " fadeInUp";
-			contactText[4].className += " fadeInUp";
-		}
-
-	} else if (window.innerWidth >= 376 && window.innerWidth <= 667) { // mobile | landscape
-
-		if (window.pageYOffset > 250) {
-			aboutHead.className += " fadeInUp";
-			aboutText[0].className += " fadeInUp";
-			aboutText[1].className += " fadeInUp";
-			aboutText[2].className += " fadeInUp";
-		}
-
-		if (window.pageYOffset > 550) {
-			workHead.className += " fadeInUp";
-			workText[0].className += " fadeInUp";
-			workText[1].className += " fadeInUp";
-			workText[2].className += " fadeInUp";
-			workText[3].className += " fadeInUp";
-			workText[4].className += " fadeInUp";
-			workText[5].className += " fadeInUp";
-		}
-
-		if (window.pageYOffset > 950) {
-			contactHead.className += " fadeInUp";
-			contactText[0].className += " fadeInUp";
-			contactText[1].className += " fadeInUp";
-			contactText[2].className += " fadeInUp";
-			contactText[3].className += " fadeInUp";
-			contactText[4].className += " fadeInUp";
-		}
-
-	}
-
-} // closes: function fadeInText()
-
-window.addEventListener("load", flipLanding, false);
-window.addEventListener("load", animeLinks, false);
-window.addEventListener("scroll", fadeInText, false);
+window.addEventListener("load", fadeIn, false);
