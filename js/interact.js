96278fbc7e4d98d32dc20efead3613032a628bd3
Ye Shin <yeheonshin@172-18-39-68.DYNAPOOL.NYU.EDU>
2017-05-14 03:35:12 -0400

    Redesigned Portfolio
    Completely redesigned portfolio

--- js/interact.js
+++ js/interact.js
@@ -1,21 +1,12 @@
-// JavaScript for Ye Heon Shin's Portfolio
+// Ye Heon Shin's JavaScript for his Portfolio
 
-function fadeIn() {
+function slideIn() {
 
-	var container, containerJotIt, pageLink;
+	var container = document.getElementById('container');
 
-	containerWork = document.querySelector("#container-work");
-	containerJotIt = document.querySelector("#container-jot-it");
-	pageLink = document.querySelector(".page-link")
-
-	if (document.title == "Ye Shin | Designer" || document.title == "Ye Shin | Designer | Work") {
-		containerWork.className = "fadeInUp";
-		pageLink.className += " fadeIn";
-	} else {
-		containerJotIt.className = "fadeInUp";
-		pageLink.className += " fadeIn";
-	}
+	container.className = "slideUp";
 
 }
 
-window.addEventListener("load", fadeIn, false);
+window.addEventListener("load", slideIn, false);
+
