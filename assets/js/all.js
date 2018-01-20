cdf7bbcafeca63c82006cc8d97d72a9ce040d02f
Ye Heon Shin <yeshin316@gmail.com>
2018-01-20 18:27:52 -0500

    Portfolio V2

---
+++ assets/js/all.js
@@ -0,0 +1,277 @@
+
+
+
+// ====================
+// Header Scroll Effect
+// ====================
+
+var header = document.querySelector('header');
+var headerNav = document.querySelectorAll('a.nav');
+
+
+
+window.addEventListener('scroll', function() {
+
+  if (document.title === "Ye Shin • Dropmark") {
+
+    if (window.innerWidth >= 650) {
+
+      if (window.pageYOffset >= 640) {
+
+        header.style.backgroundColor = "white";
+
+        for (i = 0; i < headerNav.length; i++) {
+
+          headerNav[i].style.color = "#111111";
+
+        }
+
+      } else {
+
+          header.style.backgroundColor = "#4BE1E2";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "white";
+
+          }
+
+        }
+
+      } else {
+
+        if (window.pageYOffset >= 440) {
+
+          header.style.backgroundColor = "white";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "#111111";
+
+          }
+
+        } else {
+
+            header.style.backgroundColor = "#4BE1E2";
+
+            for (i = 0; i < headerNav.length; i++) {
+
+              headerNav[i].style.color = "white";
+
+            }
+
+          }
+
+        }
+
+  } else if (document.title === "Ye Shin • Venmo Groups") {
+
+    if (window.innerWidth >= 650) {
+
+      if (window.pageYOffset >= 640) {
+
+        header.style.backgroundColor = "white";
+
+        for (i = 0; i < headerNav.length; i++) {
+
+          headerNav[i].style.color = "#111111";
+
+        }
+
+      } else {
+
+          header.style.backgroundColor = "#3C95CD";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "white";
+
+          }
+
+        }
+
+      } else {
+
+        if (window.pageYOffset >= 440) {
+
+          header.style.backgroundColor = "white";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "#111111";
+
+          }
+
+        } else {
+
+            header.style.backgroundColor = "#3C95CD";
+
+            for (i = 0; i < headerNav.length; i++) {
+
+              headerNav[i].style.color = "white";
+
+            }
+
+          }
+
+        }
+
+  } else if (document.title === "Ye Shin • Mindfull") {
+
+    if (window.innerWidth >= 650) {
+
+      if (window.pageYOffset >= 640) {
+
+        header.style.backgroundColor = "white";
+
+        for (i = 0; i < headerNav.length; i++) {
+
+          headerNav[i].style.color = "#111111";
+
+        }
+
+      } else {
+
+          header.style.backgroundColor = "#0173FF";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "white";
+
+          }
+
+        }
+
+      } else {
+
+        if (window.pageYOffset >= 440) {
+
+          header.style.backgroundColor = "white";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "#111111";
+
+          }
+
+        } else {
+
+            header.style.backgroundColor = "#0173FF";
+
+            for (i = 0; i < headerNav.length; i++) {
+
+              headerNav[i].style.color = "white";
+
+            }
+
+          }
+
+        }
+
+  } else if (document.title === "Ye Shin • Starbucks Redesign") {
+
+      if (window.innerWidth >= 650) {
+
+        if (window.pageYOffset >= 640) {
+
+          header.style.backgroundColor = "white";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "#111111";
+
+          }
+
+        } else {
+
+            header.style.backgroundColor = "#34B881";
+
+            for (i = 0; i < headerNav.length; i++) {
+
+              headerNav[i].style.color = "white";
+
+            }
+
+          }
+
+        } else {
+
+          if (window.pageYOffset >= 440) {
+
+            header.style.backgroundColor = "white";
+
+            for (i = 0; i < headerNav.length; i++) {
+
+              headerNav[i].style.color = "#111111";
+
+            }
+
+          } else {
+
+              header.style.backgroundColor = "#34B881";
+
+              for (i = 0; i < headerNav.length; i++) {
+
+                headerNav[i].style.color = "white";
+
+              }
+
+            }
+
+          }
+
+  } else if (document.title === "Ye Shin • Poster Designs") {
+
+      if (window.innerWidth >= 650) {
+
+        if (window.pageYOffset >= 640) {
+
+          header.style.backgroundColor = "white";
+
+          for (i = 0; i < headerNav.length; i++) {
+
+            headerNav[i].style.color = "#111111";
+
+          }
+
+        } else {
+
+            header.style.backgroundColor = "#FAFAFA";
+
+            for (i = 0; i < headerNav.length; i++) {
+
+              headerNav[i].style.color = "#111111";
+
+            }
+
+          }
+
+        } else {
+
+          if (window.pageYOffset >= 440) {
+
+            header.style.backgroundColor = "white";
+
+            for (i = 0; i < headerNav.length; i++) {
+
+              headerNav[i].style.color = "#111111";
+
+            }
+
+          } else {
+
+              header.style.backgroundColor = "#FAFAFA";
+
+              for (i = 0; i < headerNav.length; i++) {
+
+                headerNav[i].style.color = "#111111";
+
+              }
+
+            }
+
+          }
+
+        }
+
+}, false);
