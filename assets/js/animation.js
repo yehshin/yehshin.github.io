d0203f7103827c53efd9cf20ce3081aee6bda68f
Ye Heon Shin <yeshin316@gmail.com>
2019-07-30 00:49:52 -0400

    anime

---
+++ assets/js/animation.js
@@ -0,0 +1,38 @@
+
+
+
+// =======
+// Fade In
+// =======
+
+window.addEventListener('load', function() {
+
+    var body = document.querySelector('body');
+
+    body.style.opacity = 1;
+
+}, false)
+
+
+// =======
+// Fade In
+// =======
+
+var nextProject = document.querySelectorAll(`a.next-project`);
+var nextProjectThumbnail = document.querySelectorAll(`img.next-project-thumbnail`);
+
+nextProject[0].addEventListener(`mouseenter`, function() {
+
+  nextProjectThumbnail[0].style.filter = "brightness(0)";
+  nextProjectThumbnail[0].style.WebkitFilter = "brightness(0)";
+
+
+}, false);
+
+nextProject[0].addEventListener(`mouseleave`, function() {
+
+  nextProjectThumbnail[0].style.filter = "brightness(0.4)";
+  nextProjectThumbnail[0].style.WebkitFilter = "brightness(0.4)";
+
+
+}, false);
