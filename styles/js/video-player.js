6ba83e9cf3e33f2eae9be31c09a2c978e4bcf85d
Ye Heon Shin <yeshin316@gmail.com>
2021-08-27 16:33:18 -0400

    play/pause functionality for videos

--- styles/js/video-player.js
+++ styles/js/video-player.js
@@ -7,96 +7,103 @@
 
 
 
-// var videoPlayButton,
-// 	videoWrapper = document.getElementsByClassName('col-1-video')[0],
-//     video = document.getElementsByTagName('video')[0],
-//     videoMethods = {
-//         renderVideoPlayButton: function() {
-//             if (videoWrapper.contains(video)) {
-// 				this.formatVideoPlayButton()
-//                 video.classList.add('has-media-controls-hidden')
-//                 videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
-//                 videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
-//             }
-//         },
+// Global Variables
 
-//         formatVideoPlayButton: function() {
-//             videoWrapper.insertAdjacentHTML('beforeend', '\
-//                 <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
-//                     <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
-//                     <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
-//                 </svg>\
-//             ')
-//         },
-
-//         hideVideoPlayButton: function() {
-//             video.play()
-//             videoPlayButton.classList.add('is-hidden')
-//             video.classList.remove('has-media-controls-hidden')
-//             video.setAttribute('controls', 'controls')
-//         }
-// 	}
-
-// videoMethods.renderVideoPlayButton()
+var playButton = document.querySelectorAll(`button.play-button`);
+var pauseButton = document.querySelectorAll(`button.pause-button`);
+var video = document.querySelectorAll(`video`);
+var overlay = document.querySelectorAll(`div.video-overlay`);
 
 
 
+// Functions - Video Opacity
 
+for(var i=0; i<overlay.length; i++){
+  overlay[i].addEventListener("mouseenter",mouseEnter(i));
+}
 
+for(var i=0; i<playButton.length; i++){
+  playButton[i].addEventListener("mouseenter",mouseEnter(i));
+}
 
+function mouseEnter(i) {
 
+  return function() {
 
+    overlay[i].style.opacity = "0.4";
 
-
-var playButton = document.getElementById("play-button");
-// var video = document.getElementById("video-1")
-
-// Event listener for the play/pause button
-playButton.addEventListener("click", function() {
-
-  if (video.paused == true) {
+    if (video[i].paused == true) {
     
-    // Play the video
-    video.play();
+      playButton[i].style.opacity = "1";
+  
+      } else {
+  
+      playButton[i].style.opacity = "0";
+      pauseButton[i].style.opacity = "1";
+  
+      }
 
-    // Update the button text to 'Pause'
-    playButton.innerHTML = "Pause";
+ };
+  
+}
 
-  } else {
+for(var i=0; i<overlay.length; i++){
+  overlay[i].addEventListener("mouseleave",mouseLeave(i));
+}
 
-    // Pause the video
-    video.pause();
+function mouseLeave(i) {
 
-    // Update the button text to 'Play'
-    playButton.innerHTML = "Play";
+  return function() {
 
-  }
+    overlay[i].style.opacity = "0";
 
-});
+    if (video[i].paused == true) {
+    
+      playButton[i].style.opacity = "1";
+  
+      } else {
+
+      playButton[i].style.opacity = "0";
+      pauseButton[i].style.opacity = "0";
+  
+      }
+
+ };
+
+}
+
+// Functions - Play / Pause
+
+for(var i=0; i<overlay.length; i++){
+  overlay[i].addEventListener("click",clickPlay(i));
+}
+
+for(var i=0; i<playButton.length; i++){
+  playButton[i].addEventListener("click",clickPlay(i));
+}
+
+function clickPlay(i) {
+
+  return function() {
+
+    if (video[i].paused == true) {
+    
+    playButton[i].style.opacity = "0";
+    pauseButton[i].style.opacity = "1";
+    video[i].play()
+
+    } else {
+
+    playButton[i].style.opacity = "1";
+    pauseButton[i].style.opacity = "0";
+    video[i].pause()
+
+    }
+
+ };
+
+}
 
 
 
 
-
-
-
-// $( document ).ready(function() {
-//     var ctrlVideo = document.getElementById("video"); 
-     
-//     $('button').click(function(){
-//       if ($('button').hasClass("play-button")){
-        
-//             ctrlVideo.play();
-        
-//         $('button').html("Pause");
-//         $('button').toggleClass("play-button");
-//       } else {
-        
-//             ctrlVideo.pause();
-        
-//         $('button').html("play");
-//         $('button').toggleClass("play-button");
-//       }
-//     });
-     
-// });
